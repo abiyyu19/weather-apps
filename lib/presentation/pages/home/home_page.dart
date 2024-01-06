15e5d7910022326e7a9a3249b6a746c1ac936e0e
abiyyu19 <mhmdabiyyu19@gmail.com>
2024-01-06 09:32:54 +0700

    update home page

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:weather_apps/model/weather_model.dart';
 import 'package:weather_apps/presentation/widgets/weather_card.dart';
 import 'package:weather_apps/services/firebase_service.dart';
 import 'package:weather_apps/services/weather_service.dart';
@@ -19,83 +20,80 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser;
 
+  Weather? _weather;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _loadWeather();
+  }
+
+  Future<void> _loadWeather() async {
+    final weather = await fetchWeather();
+    setState(() {
+      _weather = weather;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     log('ada isinya ga? ${user?.email}');
     return Scaffold(
-      // backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Weather App'),
-        actions: [
-          IconButton(
-            onPressed: () async {
-              signOut().then(
-                (value) => context.goNamed('login'),
+        // backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: const Text('Weather App'),
+          actions: [
+            IconButton(
+              onPressed: () async {
+                signOut().then(
+                  (value) => context.goNamed('login'),
+                );
+              },
+              icon: const Icon(Icons.exit_to_app),
+            )
+          ],
+        ),
+        body: Builder(
+          builder: (context) {
+            if (_weather == null) {
+              return const Center(
+                child: CircularProgressIndicator(),
               );
-            },
-            icon: const Icon(Icons.exit_to_app),
-          )
-        ],
-      ),
-      body: FutureBuilder(
-        future: fetchWeather(),
-        builder: (context, snapshot) {
-          // log(snapshot.data!.city.name);
-          if (snapshot.hasData &&
-              snapshot.connectionState == ConnectionState.done) {
-            var cuaca = snapshot.data!;
+            } else {
+              var cuaca = _weather!;
 
-            var formatter = DateFormat('EEE, MMM dd, yyyy h:mm a');
+              var formatter = DateFormat('EEE, MMM dd, yyyy h:mm a');
 
-            return Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8),
-              child: ListView.separated(
-                separatorBuilder: (context, index) => Divider(
-                  color: Colors.grey[600],
-                ),
-                itemCount: cuaca.list.length,
-                itemBuilder: (BuildContext context, int index) {
-                  var iconUrl =
-                      "http://openweathermap.org/img/w/${cuaca.list[index].weather[0].icon}.png";
-                  log(iconUrl);
-                  return WeatherCard(
-                    icon: Image.network(iconUrl),
-                    date: formatter.format(cuaca.list[index].dtTxt),
-                    weather: cuaca.list[index].weather[0].main,
-                    temp: cuaca.list[index].main.temp,
-                    onPressed: () {
-                      context.goNamed(
-                        'weather_details',
-                        extra: cuaca.list[index],
-                      );
-                    },
-                  );
-                },
-              ),
-            );
-          } else if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else {
-            return Padding(
-              padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.05,
-              ),
-              child: const Center(
-                child: Text(
-                  'Data Cuaca gagal',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                    fontSize: 16,
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: ListView.separated(
+                  separatorBuilder: (context, index) => Divider(
+                    color: Colors.grey[600],
                   ),
+                  itemCount: cuaca.list.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    var iconUrl =
+                        "http://openweathermap.org/img/w/${cuaca.list[index].weather[0].icon}.png";
+                    log(iconUrl);
+                    return WeatherCard(
+                      icon: Image.network(iconUrl),
+                      date: formatter.format(cuaca.list[index].dtTxt),
+                      weather: cuaca.list[index].weather[0].main,
+                      temp: cuaca.list[index].main.temp,
+                      onPressed: () {
+                        context.goNamed(
+                          'weather_details',
+                          extra: cuaca.list[index],
+                        );
+                      },
+                    );
+                  },
                 ),
-              ),
-            );
-          }
-        },
-      ),
-    );
+              );
+            }
+          },
+        ));
   }
 }
